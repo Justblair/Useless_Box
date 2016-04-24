9dcd941d8120bc8c51323c832adc2ce557a095f2
Justblair <mail@justblair.co.uk>
2016-04-24 18:06:41 +0100

    PutChar function now working
    Got the putChar function working properly.

--- uselessbox/uselessbox.ino
+++ uselessbox/uselessbox.ino
@@ -13,7 +13,7 @@
 //#define mqtt_user ""
 //#define mqtt_password ""
 
-enum characters {A,r,d,u,i,n,o};
+enum characters { A, r, d, u, i, n, o };
 
 /*
  Now we need a LedControl to work with.
@@ -34,97 +34,99 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 const int motorAPWM = 15;
-const int motorB    = 11;
+const int motorB = 11;
 const int switchPin = 9;
 
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("Hello");
-  wakeMAX72XX();
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-  pinMode (motorAPWM, OUTPUT);
-  pinMode (motorB, OUTPUT);
-  pinMode (switchPin, INPUT_PULLUP);
-  attachInterrupt (switchPin, switchChange, CHANGE);
+	Serial.begin(9600);
+	Serial.println("Hello");
+	wakeMAX72XX();
+	setup_wifi();
+	client.setServer(mqtt_server, 1883);
+	client.setCallback(callback);
+	//pinMode(motorAPWM, OUTPUT);
+	//pinMode(motorB, OUTPUT);
+	//pinMode(switchPin, INPUT_PULLUP);
+	//attachInterrupt(switchPin, switchChange, CHANGE);
 }
 
 
 void switchChange(){
-  // do switchy stuff here
-  
+	// do switchy stuff here
+
 }
 
 void loop() {
-    if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-  writeScreen();
-  delay(delaytime);
-  for (int i = 0; i < 24; i++) {
-    memset(buf, 0x0, 64);
-     putSprite(i, 9, 4, 5,  r);
-     putChar(i,0,'A');
-     writeScreen();
-    delay(delaytime);
-  }
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
+	writeScreen();
+	delay(delaytime);
+	for (int i = 0; i < 24; i++) {
+		char ci[2];
+		memset(buf, 0x0, 64);
+		putSprite(i, 9, 4, 5, A);
+		itoa(i, ci, 10);
+		putChar(i, 0, ci[0]);
+		writeScreen();
+		delay(delaytime);
+	}
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
- Serial.print("Message arrived [");
- Serial.print(topic);
- Serial.print("] ");
-  if (strcmp(topic, "uselessBox") == 0){
-    for (int i=0;i<length;i++) {
-      putChar(i*6, 0, payload[i]);
-    }  
-  }
- 
- for (int i=0;i<length;i++) {
-  char receivedChar = (char)payload[i];
-  Serial.print(receivedChar);
-  Serial.println();
-}
+	Serial.print("Message arrived [");
+	Serial.print(topic);
+	Serial.print("] ");
+	if (strcmp(topic, "uselessBox") == 0){
+		for (int i = 0; i < length; i++) {
+			putChar(i * 6, 0, payload[i]);
+		}
+	}
+
+	for (int i = 0; i < length; i++) {
+		char receivedChar = (char)payload[i];
+		Serial.print(receivedChar);
+		Serial.println();
+	}
 }
 
 void wakeMAX72XX(){
-  /*
-   The MAX72XX is in power-saving mode on startup,
-   we have to do a wakeup call
-   */
-  for (int i = 0; i < 6; i++) {
-    lc.shutdown(i, false);
-    /* Set the brightness to a medium values */
-    lc.setIntensity(i, 8);
-    /* and clear the display */
-    lc.clearDisplay(i);
-  }
+	/*
+	 The MAX72XX is in power-saving mode on startup,
+	 we have to do a wakeup call
+	 */
+	for (int i = 0; i < 6; i++) {
+		lc.shutdown(i, false);
+		/* Set the brightness to a medium values */
+		lc.setIntensity(i, 8);
+		/* and clear the display */
+		lc.clearDisplay(i);
+	}
 }
 
 void setup_wifi() {
-  delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(wifi_ssid);
+	delay(10);
+	// We start by connecting to a WiFi network
+	Serial.println();
+	Serial.print("Connecting to ");
+	Serial.println(wifi_ssid);
 
-  WiFi.begin(wifi_ssid, wifi_password);
+	WiFi.begin(wifi_ssid, wifi_password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.println("WiFi connected");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
 // This function works for a single sprite so far WIP
-void putSprite(int x, int y, int width, int height,  int sp) {
+void putSprite(int x, int y, int width, int height, int sp) {
 	for (int i = 0; i < height; i++) {
 		buf[i + y] = (uint32_t(sprite[sp][i])) << (23 - x - width);
 	}
@@ -132,14 +134,23 @@ void putSprite(int x, int y, int width, int height,  int sp) {
 
 // This function works for a single sprite so far WIP
 void putChar(int x, int y, char sp) {
-  int charNo = sp - 32;  // starting point from ascii array
-  int width = arial_8ptDescriptors[charNo][0];
-  int arrayPos = arial_8ptDescriptors[charNo][1];
-  for (int i = 0; i < 8; i++) {  // add 8 lines into the buffer
-    if (width == 1){
-        buf[i + y] ^= (uint32_t(arial_8ptBitmaps[charNo + i])) << (23 - x - width);
-    }
-  }
+	int charNo = int(sp) - 32;  // starting point from ascii array
+	//Serial.print("CharNo: ");
+	//Serial.println(charNo, DEC);
+
+	int width = arial_8ptDescriptors[charNo - 1][0];
+	//Serial.print("Width: ");
+	//Serial.println(width, DEC);
+
+	int arrayPos = arial_8ptDescriptors[charNo - 1][1];
+	//Serial.print("Array pos: ");
+	//Serial.println(arrayPos, DEC);
+
+	for (int i = 0; i < 8; i++) {  // add 8 lines into the buffer
+		if (width == 1){
+			buf[i + y] ^= (uint32_t(arial_8ptBitmaps[arrayPos + i])) << (23 - x - width);
+		}
+	}
 }
 
 // Write the contents of the buffer.... 
@@ -153,23 +164,24 @@ void writeScreen() {
 }
 
 void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    // If you do not want to use a username and password, change next line to
-    // if (client.connect("ESP8266Client")) {
-    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
-      Serial.println("connected");
-      client.subscribe("uselessBox");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+	// Loop until we're reconnected
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+		// Attempt to connect
+		// If you do not want to use a username and password, change next line to
+		// if (client.connect("ESP8266Client")) {
+		if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+			Serial.println("connected");
+			client.subscribe("uselessBox");
+		}
+		else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			// Wait 5 seconds before retrying
+			delay(5000);
+		}
+	}
 }
 
 
